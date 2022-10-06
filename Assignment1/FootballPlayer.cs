7fdb64cd425e8113fa38f8e5e5a3b0fb6e7f4637
ghimiresabin147@gmail.com <ghimiresabin147@gmail.com>
2022-10-06 08:49:58 +0200

    Add project files.

---
+++ Assignment1/FootballPlayer.cs
@@ -0,0 +1,54 @@
+﻿namespace Assignment1
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public int ShirtNumber { get; set; }
+
+        public void ValidateId()
+        {
+            if (Id < 1)
+            {
+                throw new ArgumentOutOfRangeException("Id must be a positive number!");
+            }
+        }
+
+        public void ValidateName()
+        {
+            if (Name == null)
+            {
+                throw new ArgumentNullException("you need to write Name");
+            }
+            else if (Name.Length < 2)
+            {
+                throw new ArgumentOutOfRangeException("Name needs to be atleast 2 character");
+            }
+        }
+
+        public void ValidateAge()
+        {
+            if (Age < 1)
+            {
+                throw new ArgumentOutOfRangeException("you are not old enough");
+            }
+        }
+        
+        public void ValidateShirtNumber()
+        {
+            if (ShirtNumber < 1 || ShirtNumber > 99)
+            {
+                throw new ArgumentOutOfRangeException("Invalid Shirt Number");
+            }
+        }
+
+        public override string ToString()
+        {
+            return "The player " +Name+ " has id " +Id+ " is " +Age+ " years old and has shirtnumber "+ShirtNumber;
+        }
+
+
+
+    }
+}
---
+++ Assignment1Tests/FootballPlayerTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Assignment1.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        FootballPlayer p1 = new FootballPlayer() { Id = 1, Name = "Sabin", Age = 21, ShirtNumber = 1 };
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            string nam = p1.ToString();
+            Assert.AreEqual("The player Sabin has id 1 is 21 years old and has shirtnumber 1", nam);
+        }
+        
+        [TestMethod()]
+        public void NameLessThan2CharacterTest()
+        {
+            
+            FootballPlayer p2 = new FootballPlayer() { Id = 2, Name = "A", Age = 20, ShirtNumber = 2 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => p2.ValidateName());
+        }
+        [TestMethod()]
+        public void NegativeAgeTest()
+        {
+            FootballPlayer p3 = new FootballPlayer() { Id = 3, Name = "Adeel", Age = 0, ShirtNumber = 3 };
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => p3.ValidateAge());
+        }
+        [TestMethod()]
+        public void ShirtNumberLessThan0Test()
+
+        {
+            FootballPlayer p4 = new FootballPlayer() { Id = 4, Name = "Sabin", Age = 22, ShirtNumber = -1 };
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => p4.ValidateShirtNumber());
+        }
+        [TestMethod()]
+        public void ShirtNumberGreaterThan99Test()
+        {
+            FootballPlayer p5 = new FootballPlayer() { Id = 5, Name = "Saugat", Age = 25, ShirtNumber = 100 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => p5.ValidateShirtNumber());
+        }
+    }
+}
